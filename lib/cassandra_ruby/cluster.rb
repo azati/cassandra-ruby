f7d6fd96b7924c40ed62dd5c99fe75391acf8249
root <root@markelov-a-ubuntu.(none)>
2010-05-14 17:17:07 +0300

    CassandraRuby::Cluster class added. The class allowed to work with Cassandra as a cluster.

---
+++ lib/cassandra_ruby/cluster.rb
@@ -0,0 +1,77 @@
+module CassandraRuby
+  class Cluster
+    attr_reader :name, :endpoints
+
+    def initialize(host_or_hosts, options = {})
+      @port = options[:port] || 9160
+      @endpoints = host_or_hosts.is_a?(Array) ? host_or_hosts.dup : [host_or_hosts]
+    end
+
+    def connect
+      cluster_safe_cycle do
+        @name      = @client.describe_cluster_name
+        @endpoints = describe_endpoints
+      end
+    end
+
+    def disconnect
+      if @connection && @connection.open?
+        @connection.close
+      end
+      @connection = nil
+      @client     = nil
+    end
+
+    def reconnect
+      disconnect
+      connect
+    end
+
+    Thrift::Client.instance_methods(false).each do |method|
+      define_method(method) do |*args|
+        cluster_safe_cycle { @client.send(method, *args) }
+      end
+    end
+
+    protected
+
+    def cluster_safe_cycle
+      connect_to(@endpoints.first) unless @connection && @connection.open?
+      result = yield
+    rescue ::Thrift::TransportException
+      disconnect
+      @endpoints.shift
+      raise if @endpoints.empty?
+      retry
+    else
+      result
+    end
+
+    def connect_to(host)
+      @connection = ::Thrift::Socket.new(host, @port)
+      protocol = ::Thrift::BinaryProtocol.new(@connection)
+      @client = Thrift::Client.new(protocol)
+      @connection.open
+    rescue ::Thrift::TransportException
+      disconnect
+      raise
+    else
+      @client
+    end
+
+    def describe_endpoints
+      @client.describe_keyspaces.inject([]) do |endpoints, keyspace|
+        unless keyspace == 'system'
+          endpoints |= describe_keyspace_endpoints(keyspace)
+        end
+        endpoints
+      end
+    end
+
+    def describe_keyspace_endpoints(keyspace)
+      @client.describe_ring(keyspace).inject([]) do |endpoints, token_range|
+        endpoints |= token_range.endpoints
+      end
+    end
+  end
+end
