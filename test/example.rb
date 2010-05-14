77df814a15e2d68b3c994be9d04e2764430b1898
Alexander Markelov <alexander.markelov@azati.com>
2010-05-14 17:20:42 +0300

    Example of using CassandraRuby::Cluster class

--- test/example.rb
+++ test/example.rb
@@ -1,19 +1,9 @@
 require 'rubygems'
 require '../lib/cassandra_ruby'
 
-transport    = Thrift::Socket.new('localhost', 9160)
-#transport = Thrift::BufferedTransport.new(socket)
-protocol  = Thrift::BinaryProtocol.new(transport)
-cassandra = CassandraRuby::Thrift::Client.new(protocol)
-
-def benchmark
-  t = Time.now
-  yield
-  puts Time.now - t
-end
-
-transport.open
-100.times do
-  benchmark { cassandra.describe_ring('Keyspace1') }
-end
-transport.close
+cassandra = CassandraRuby::Cluster.new('192.168.92.198')
+cassandra.connect
+# 5.times { |i| puts 5 - i; sleep(1) }
+cassandra.describe_ring('Keyspace1').each { |i| puts i.inspect }
+puts cassandra.endpoints
+cassandra.disconnect
