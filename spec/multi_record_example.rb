6d669500d1914e9b08dd01d9425f89e8b5902faa
Artemij <artem.zakolodkin@azati.com>
2010-06-09 16:14:14 +0300

    Tests

--- spec/multi_record_example.rb
+++ spec/multi_record_example.rb
@@ -23,28 +23,127 @@
 require File.expand_path(File.join('.', 'helpers', 'spec_helper'), File.dirname(__FILE__))
 
 describe CassandraRuby::MultiRecord do
-  before(:each) do
-    @object = CassandraRuby::MultiRecord.new(@ks, '')
-  end
   
   it_should_behave_like "initialized record"
-
+  it_should_behave_like "fixtures loaded"
+  
+  before(:each) do
+    @keys = [@data.keys.first, @data.keys.last] 
+    @object = CassandraRuby::MultiRecord.new(@ks, @keys)
+  end
+  
+  it "#{described_class} should return 'keys' value" do
+    @object.keys.should == @keys
+  end
+  
   it "#{described_class} should implement 'get'" do
-    pending("not implemented") do
-      raise   
-    end    
+    result = @object.get('Standard1')
+    result.empty?.should_not == true
+    result.each do |c|
+      @data.keys.include?(c[0]).should == true
+      c[1].class.should == Array
+      c[1].each do |item|
+        item.class.should == CassandraRuby::Thrift::Column
+      end
+    end
+    
+    result = @object.get('Super1')
+    result.empty?.should_not == true
+    result.each do |c|
+      @data.keys.include?(c[0]).should == true
+      c[1].class.should == Array
+      c[1].each do |item|
+        item.class.should == CassandraRuby::Thrift::SuperColumn
+      end
+    end
   end
   
   it "#{described_class} should implement 'insert'" do
-    pending("not implemented") do
-      raise   
-    end    
+    @object.insert('Standard1', nil, {'InsertTest1'=>'aaa'}, Time.now)
+    result = @object.get('Standard1', nil, 'InsertTest1')
+    result.keys.count.should == @keys.count
+    result.each do |key, data|
+      @keys.include?(key).should == true
+      data.class.should == CassandraRuby::Thrift::Column
+      data.name.should == 'InsertTest1'
+    end
+    
+    @object.insert('Super1', 'InsertSuperColumn', {'InsertTest1' => 'insert data 1'}, Time.now)
+    result = @object.get('Super1', 'InsertSuperColumn')
+    result.keys.count.should == @keys.count
+    result.each do |key, data|
+      @keys.include?(key).should == true
+      data.class.should == CassandraRuby::Thrift::SuperColumn
+      data.name.should == 'InsertSuperColumn'
+    end
+    
+    
+    #Clean the storage. USE NATIVE THRIFT API ONLY!!!
+    column_path = CassandraRuby::Thrift::ColumnPath.new(:column_family => 'Standard1',
+        :super_column  => nil,
+        :column        => 'InsertTest1'
+    )
+    time = Time.now
+    time = time.to_i * 1_000_000 + time.usec
+    @keys.each do |key|
+      @object.keyspace.client.remove(@object.keyspace.name, key, column_path, time, CassandraRuby::Thrift::ConsistencyLevel::const_get(:one.to_s.upcase) )  
+    end
+    
+    column_path = CassandraRuby::Thrift::ColumnPath.new(:column_family => 'Super1',
+        :super_column  => 'InsertSuperColumn',
+        :column        => nil
+    )
+    @keys.each do |key|
+      @object.keyspace.client.remove(@object.keyspace.name, key, column_path, time, CassandraRuby::Thrift::ConsistencyLevel::const_get(:one.to_s.upcase) )
+    end
+    #Verify if all was removed
+    result = @object.get('Standard1', nil, 'InsertTest1')
+    @keys.each do |key|
+      result[key].should == nil  
+    end
+    
+    result = @object.get('Super1', 'InsertSuperColumn')
+    @keys.each do |key|
+      result[key].should == nil
+    end
   end
   
   it "#{described_class} should implement 'remove'" do
-    pending("not implemented") do
-      raise   
-    end    
+    #Fill the storage. USE NATIVE THRIFT API ONLY!!!
+    column_path = CassandraRuby::Thrift::ColumnPath.new(
+        :column_family => 'Standard1',
+        :super_column  => nil,
+        :column        => 'RemoveTest1'
+    )
+    time = Time.now
+    time = time.to_i * 1_000_000 + time.usec
+    @keys.each do |key|
+      @object.keyspace.client.insert(@object.keyspace.name, key, column_path, 'remove data 1', time, CassandraRuby::Thrift::ConsistencyLevel::const_get(:one.to_s.upcase))
+    end
+    
+    column_path = CassandraRuby::Thrift::ColumnPath.new(
+        :column_family => 'Super1',
+        :super_column  => 'RemoveSuperTest',
+        :column        => 'column'
+    )
+    time = Time.now
+    time = time.to_i * 1_000_000 + time.usec
+    @keys.each do |key|
+      @object.keyspace.client.insert(@object.keyspace.name, key, column_path, 'super remove data 1', time, CassandraRuby::Thrift::ConsistencyLevel::const_get(:one.to_s.upcase))  
+    end
+    
+    #Test itself
+    @object.remove('Standard1', nil, 'RemoveTest1', Time.now)
+    result = @object.get('Standard1', nil, 'RemoveTest1')
+    @keys.each do |key|
+      result[key].should == nil
+    end
+    
+    @object.remove('Super1', 'RemoveSuperTest', 'column', Time.now)
+    result = @object.get('Super1', 'RemoveSuperTest')
+    @keys.each do |key|
+      result[key].should == nil
+    end
   end
   
 end
